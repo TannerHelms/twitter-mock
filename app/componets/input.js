c3aee4ce21e26f61ec93aada26472ecf55ae80e8
TannerHelms <tannerhelms01@gmail.com>
2024-05-02 11:23:23 -0600

    updated feed componet

---
+++ app/componets/input.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { CiImageOn } from "react-icons/ci";
+import { MdOutlineEmojiEmotions } from "react-icons/md";
+export default function Input() {
+    return (
+        <div className="flex space-x-4 border-b border-gray-200 p-3">
+            <Image className="rounded-full h-11 w-11 cursor-pointer hover:brightness-95" width={50} height={50} src="https://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png" />
+            <div className="w-full divide-y divide-gray-200">
+                <textarea name="content" className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700" cols="30" rows="2" placeholder="What's happening?"></textarea>
+                <div className="flex justify-between pt-3">
+                    <div className="flex space-x-3">
+                        <div className="hover:bg-sky-100 p-2 rounded-full cursor-pointer">
+                            <CiImageOn className="h-6 w-6 text-sky-500" />
+                        </div>
+                        <div className="hover:bg-sky-100 p-2 rounded-full cursor-pointer">
+                            <MdOutlineEmojiEmotions className="h-6 w-6 text-sky-500" />
+                        </div>
+                    </div>
+                    <button className="btn-blue w-20 h-8 p-0">Tweet</button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
